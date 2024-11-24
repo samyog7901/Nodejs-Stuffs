377c35420ef93908589a4163c8a7a3e28964d292
samyog7901 <samyogkhadka247@gmail.com>
2024-11-24 22:51:35 +0545

    basics of nodejs

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require('express') //express lai node_module  nata nikalera expr
 const app = express()
 
 
-app.listen(3000,()=>{
-    console.log("Prem se Bolo Radhe Radhe!")
-})
+app.set('view engine','ejs')//expresslai maile ejs use garna laako 
+                          // yesko laagi required sabai environments(configuration) set garde vaneko 
 
-app.get('/',(req,res)=>{
-   res.send("Radhe....!")
+
+
+app.get('/home',(req,res)=>{
+   const data = {
+      name:'Samyog',
+      age: 21,
+      location: 'khotang'
+   }
+   res.render("home.ejs",{
+    data,
+    
+   })
 })
 
 app.get('/about',(req,res)=>{
-    res.send("This is about page.")
+    res.render("about.ejs")
+})
+
+app.listen(3000,()=>{
+    console.log("Prem se Bolo Radhe Radhe!")
 })

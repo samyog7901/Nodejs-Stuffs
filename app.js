a5066d810f319f11e0a0aec7ab0bdb59a137ce73
samyog7901 <samyogkhadka247@gmail.com>
2024-12-04 11:52:33 +0545

    create operation

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
-const express = require('express') //express lai node_module  bata nikalera express vanne vadama rakhe
-const app = express()
 require('dotenv').config()//ma dotenv use gariraxu sabai reqd config set garde vane
+const express = require('express') //express lai node_module  bata nikalera express vanne vadama rakhe
+const { blogs } = require('./model/index')
+const app = express()
+
+
+// app.use(express.json())//yo chai primary arch.ko lagi 
+app.use(express.urlencoded({extended :true}))//req.body ma data pani paryo(yo monolothic arch lai)
 app.set('view engine','ejs')//expresslai maile ejs use garna laako yesko laagi required sabai environments(configuration) set garde vaneko 
                            
 require("./model/index") //Database connection
 
-
-
-app.get('/home',(req,res)=>{
-   const data = {
-      name:'Samyog',
-      age: 21,
-      location: 'khotang'
-   }
-   const nepal = {
-    country :'Nepal',
-    continent : 'Asia',
-    nickName : 'Land of Himalayas'
-
-   }
-
-   res.render("home.ejs",{
-    ram : data,
-    laxman : nepal
-    
-   })
+app.get("/create",(req,res)=>{
+    res.render('create.ejs')//UI dekhaune code
 })
 
-app.get('/about',(req,res)=>{
-    res.render("about.ejs")
+app.post("/create",async (req,res)=>{
+   const {title, subtitle,description} = req.body
+    await blogs.create({
+        title,
+        subtitle,
+        description
+    })
+    res.send("Blog added succesfully.")
 })
 
 app.use(express.static('public/css/'))//given access to content of public/css folder
